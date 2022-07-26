827138ccf4c7a2a08f11a7b1350cf919d5fb3e14
BigMoneyBones <87784559+BigMoneyBones@users.noreply.github.com>
2022-07-26 11:27:46 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { useState } from 'react';
-import HomePage from './Pages/HomePage';
+import { useState } from "react";
+import HomePage from "./Pages/HomePage";
 
-const urlEndpoint = process.env.REACT_APP_DATABASE_URL;
-
+const urlEndpoint = process.env.REACT_APP_URL_ENDPOINT;
 
 function App() {
-  const [clientMessage, setClientMessage] = useState('');
-  const [serverMessage, setServerMessage] = useState('');
+  const [clientMessage, setClientMessage] = useState("");
+  const [serverMessage, setServerMessage] = useState("");
 
   const sendReceiveMessage = async () => {
-    console.log("client message: ", clientMessage)
+    console.log("client message: ", clientMessage);
     const response = await fetch(`${urlEndpoint}/post-message`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ clientMessage })
-    })
+      body: JSON.stringify({ clientMessage }),
+    });
     const responseJSON = await response.json();
-    setServerMessage(responseJSON.serverMessage)
-  }
-
+    setServerMessage(responseJSON.serverMessage);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          <Route index element={<HomePage
-            clientMessage={clientMessage}
-            setClientMessage={setClientMessage}
-            serverMessage={serverMessage}
-            sendReceiveMessage={sendReceiveMessage}
-          />} />
+          <Route
+            index
+            element={
+              <HomePage
+                clientMessage={clientMessage}
+                setClientMessage={setClientMessage}
+                serverMessage={serverMessage}
+                sendReceiveMessage={sendReceiveMessage}
+              />
+            }
+          />
         </Routes>
-
       </header>
     </div>
   );
